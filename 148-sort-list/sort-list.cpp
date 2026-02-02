c4eca4594d3a93e41ccd2afaf977ad9078288fd0
anjalikushwaha151103 <anjalikushwaha8009@gmail.com>
2026-02-02 19:38:40 +0530

    Time: 67 ms (6.35%) | Memory: 75.8 MB (7.25%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -10,28 +10,54 @@
  */
 class Solution {
 public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+
+
+    ListNode* merge(ListNode* lh, ListNode* rh){
+        ListNode* t1=lh;
+        ListNode* t2=rh;
+        ListNode* dnode= new ListNode(-1);
+        ListNode*temp=dnode;
+
+        while(t1!=NULL && t2!=NULL){
+            if(t1->val < t2->val){
+                temp->next=t1;
+                temp=temp->next;
+                t1=t1->next;
+            }else{
+                temp->next=t2;
+                temp=temp->next;
+                t2=t2->next;
+            }
+        }
+        if(t1){
+            temp->next=t1;
+        }
+        if(t2){
+            temp->next=t2;
+        }
+        return dnode->next;
+    }
+
+
     ListNode* sortList(ListNode* head) {
         if(head==NULL ||head->next==NULL) return head;
-        vector<int> v;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            v.push_back(temp->val);
-            temp=temp->next;
-        }
+        ListNode* middle= middleNode(head);
+        ListNode* lh=head;
+        ListNode* rh=middle->next;
+        middle->next=NULL;
 
-        temp=head;
-        sort(v.begin(),v.end());
-        int n=v.size();
-        for(int i=0;i<n;i++){
-            temp->val=v[i];
-            temp=temp->next;
-        }
-        return head;
-
-
-
-
-
+        lh=sortList(lh);
+        rh=sortList(rh);
+        return merge(lh,rh);
         
     }
 };
