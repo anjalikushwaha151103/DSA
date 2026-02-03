55637cfab2ddfc87d0898f448b667d348e75d2ca
anjalikushwaha151103 <anjalikushwaha8009@gmail.com>
2026-02-03 20:01:36 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (44.40%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,10 +11,6 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        // if(l1==NULL && l2==NULL) return NULL;
-        // if(l1==NULL) return l2;
-        // if(l2==NULL) return l1;
-
         ListNode* t1=l1;
         ListNode* t2=l2;
         ListNode* h_ans=new ListNode(-1);
