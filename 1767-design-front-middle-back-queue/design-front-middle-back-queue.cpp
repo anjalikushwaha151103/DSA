156945349011ff7bec0119565ca4ea99464fd513
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-02-11 23:52:38 +0530

    Time: 3 ms (79.40%) | Memory: 27.7 MB (19.85%) - LeetSync

---
+++ 1767-design-front-middle-back-queue/design-front-middle-back-queue.cpp
@@ -0,0 +1,95 @@
+#include <deque>
+using namespace std;
+
+class FrontMiddleBackQueue {
+private:
+    deque<int> left, right;
+
+    void rebalance() {
+        if (left.size() > right.size() + 1) {
+            right.push_front(left.back());
+            left.pop_back();
+        } 
+        else if (left.size() < right.size()) {
+            left.push_back(right.front());
+            right.pop_front();
+        }
+    }
+
+public:
+    FrontMiddleBackQueue() {}
+
+    void pushFront(int val) {
+        left.push_front(val);
+        rebalance();
+    }
+
+    void pushMiddle(int val) {
+        if (left.size() > right.size()) {
+            right.push_front(left.back());
+            left.pop_back();
+        }
+        left.push_back(val);
+    }
+
+    void pushBack(int val) {
+        right.push_back(val);
+        rebalance();
+    }
+
+    int popFront() {
+        if (left.empty() && right.empty())
+            return -1;
+
+        int val;
+        if (!left.empty()) {
+            val = left.front();
+            left.pop_front();
+        } else {
+            val = right.front();
+            right.pop_front();
+        }
+
+        rebalance();
+        return val;
+    }
+
+    int popMiddle() {
+        if (left.empty() && right.empty())
+            return -1;
+
+        int val = left.back();
+        left.pop_back();
+        rebalance();
+        return val;
+    }
+
+    int popBack() {
+        if (left.empty() && right.empty())
+            return -1;
+
+        int val;
+        if (!right.empty()) {
+            val = right.back();
+            right.pop_back();
+        } else {
+            val = left.back();
+            left.pop_back();
+        }
+
+        rebalance();
+        return val;
+    }
+};
+
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ * obj->pushFront(val);
+ * obj->pushMiddle(val);
+ * obj->pushBack(val);
+ * int param_4 = obj->popFront();
+ * int param_5 = obj->popMiddle();
+ * int param_6 = obj->popBack();
+ */
