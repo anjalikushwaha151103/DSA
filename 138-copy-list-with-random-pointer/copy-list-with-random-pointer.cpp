d5aae59f41bac2cb30e5803decc3dcded1d47440
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-02-12 11:40:26 +0530

    Time: 7 ms (52.51%) | Memory: 15.2 MB (27.30%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -31,36 +31,6 @@ public:
             temp=temp->next;
         }
         return mp[head];
-
-        // Node* dhead=new Node(-1);
-        // Node* t1=dhead;
-        // Node* temp= head;
-
-        // vector <Node*> rand;
-        // unordered_map<Node* , Node*> mp;
-        // while(temp!=NULL){
-        //     int data= temp->val;
-        //     rand.push_back(temp->random);
-        //     Node* t2=new Node(data);
-        //     mp[temp]=t2;
-        //     t1->next=t2;
-        //     t1=t1->next;
-        //     temp=temp->next;
-        // }
-
-        // t1=dhead->next;
-        // int i=0;
-        // while(t1!=NULL){
-        //     if(rand[i]!=NULL){
-        //         t1->random= mp[rand[i]];
-        //     }else{
-        //         t1->random=NULL;
-        //     }
-        //     t1=t1->next;
-        //     i++;
-        // }
-
-        // return dhead->next;
         
         
     }
