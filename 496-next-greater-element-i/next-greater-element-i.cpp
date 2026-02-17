3358adc8bd3f3bd84d1be45793ff50db7f8b5d3d
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-02-17 17:10:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (73.32%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,24 +3,49 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n1=nums1.size();
         int n2=nums2.size();
-        vector<int> nge(n1);
+        unordered_map <int,int>  mp;
+        vector<int> v;
+        stack<int> st;
 
-        for(int i=0;i<n1;i++){
-            stack<int> st;
-            int k=0;
-            for(int j=n2-1;j>=0;j--){
-                while(!st.empty() && st.top()<=nums2[j]){
-                    st.pop();
-                }
-                if(nums1[i]==nums2[j]){
-                    if(st.empty()) nge[i]=-1;
-                    else nge[i]=st.top();
-                    break;
-                    
-                }
-                st.push(nums2[j]);
+        for(int j=n2-1;j>=0;j--){
+            while(!st.empty() && st.top()<=nums2[j]){
+                st.pop();
             }
+
+            if(st.empty()) mp[nums2[j]]=-1;
+            else mp[nums2[j]]=st.top();            
+            st.push(nums2[j]);
         }
-        return nge;        
+
+        for(int el:nums1){
+            v.push_back(mp[el]);
+        }
+
+        return v;
+
+    
+
+
+        // int n1=nums1.size();
+        // int n2=nums2.size();
+        // vector<int> nge(n1);
+
+        // for(int i=0;i<n1;i++){
+        //     stack<int> st;
+        //     int k=0;
+        //     for(int j=n2-1;j>=0;j--){
+        //         while(!st.empty() && st.top()<=nums2[j]){
+        //             st.pop();
+        //         }
+        //         if(nums1[i]==nums2[j]){
+        //             if(st.empty()) nge[i]=-1;
+        //             else nge[i]=st.top();
+        //             break;
+                    
+        //         }
+        //         st.push(nums2[j]);
+        //     }
+        // }
+        // return nge;        
     }
 };
