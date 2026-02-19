67828fa801ca9fe9fae76bf5b7fca4fda69f07da
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-02-20 01:22:08 +0530

    Time: 8 ms (94.18%) | Memory: 47.1 MB (78.03%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -4,19 +4,40 @@ public:
         int n = arr.size();
         const int mod = 1e9 + 7;
         long long ans = 0;
+
+        vector<int> left(n), right(n);
         stack<int> st;
 
-        for(int i = 0; i <= n; i++) {
-            while(!st.empty() && (i == n || arr[st.top()] > arr[i])) {
-                int mid = st.top();
+        // Previous Less Element (strictly smaller)
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && arr[st.top()] > arr[i])
                 st.pop();
 
-                int left = st.empty() ? mid + 1 : mid - st.top();
-                int right = i - mid;
+            if(st.empty())
+                left[i] = i + 1;
+            else
+                left[i] = i - st.top();
 
-                ans = (ans + (long long)arr[mid] * left * right) % mod;
-            }
             st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+
+        // Next Less Element (smaller or equal)
+        for(int i = n - 1; i >= 0; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+
+            if(st.empty())
+                right[i] = n - i;
+            else
+                right[i] = st.top() - i;
+
+            st.push(i);
+        }
+
+        for(int i = 0; i < n; i++){
+            ans = (ans + (long long)arr[i] * left[i] * right[i]) % mod;
         }
 
         return ans;
