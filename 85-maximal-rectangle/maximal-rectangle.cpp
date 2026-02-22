c6b99c0bb8ec683fd4076fb18965277347a304d5
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-02-22 06:09:58 +0530

    Time: 1 ms (91.59%) | Memory: 21.2 MB (16.17%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,58 +1,64 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        
-        if (matrix.empty()) return 0;
-
-        int m = matrix[0].size();
-        vector<int> height(m, 0);
-        int maxArea = 0;
-
-        for (auto& row : matrix) {
-
-            for (int i = 0; i < m; i++) {
-                if (row[i] == '1') height[i]++;
-                else height[i] = 0;
+        if(matrix.empty()) return 0;
+        int n= matrix.size();
+        int m= matrix[0].size();
+        int max_rec=0;
+        vector<int> arr(m,0);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    arr[j]=arr[j]+1;
+                }else{
+                    arr[j]=0;
+                }
+    
             }
-
-            maxArea = max(maxArea, largestRectangleArea(height));
+            int area= largestRectangleArea(arr);
+            max_rec= max(max_rec,area);
         }
 
-        return maxArea;
+        return max_rec;
+        
+        
     }
 
     int largestRectangleArea(vector<int>& arr) {
-        int n = arr.size();
-        int max_ar = 0;
+        int max_ar=0;
+        int n=arr.size();
 
         vector<int> nse(n);
         stack<int> st;
-
-        // NSE
-        for(int i = n-1; i >= 0; i--){
-            while(!st.empty() && arr[st.top()] >= arr[i]){
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
                 st.pop();
             }
-            nse[i] = st.empty() ? n : st.top();
+
+            if(st.empty()) nse[i]=n;
+            else nse[i]=st.top();
+
             st.push(i);
         }
 
         while(!st.empty()) st.pop();
 
         vector<int> pse(n);
-
-        // PSE + area
-        for(int i = 0; i < n; i++){
-            while(!st.empty() && arr[st.top()] >= arr[i]){
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()]>=arr[i]){
                 st.pop();
             }
-            pse[i] = st.empty() ? -1 : st.top();
-            st.push(i);
 
-            int area = arr[i] * (nse[i] - pse[i] - 1);
-            max_ar = max(max_ar, area);
+            if(st.empty()) pse[i]=-1;
+            else pse[i]=st.top();
+
+            st.push(i);
+            int area= arr[i]*(nse[i]-pse[i]-1);
+            max_ar=max(area,max_ar);
+
         }
 
         return max_ar;
+        
     }
 };
