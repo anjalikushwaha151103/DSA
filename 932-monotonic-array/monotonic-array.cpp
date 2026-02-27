49f24823209d4d1402ee414efb9fadf4df850c68
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-02-28 00:40:50 +0530

    Time: 0 ms (100.00%) | Memory: 100.5 MB (34.35%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -3,7 +3,6 @@ public:
     bool isMonotonic(vector<int>& nums) {
         bool increasing = true;
         bool decreasing = true;
-
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i] > nums[i-1])
                 decreasing = false;
