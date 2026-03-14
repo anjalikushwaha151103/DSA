d4ddc6702c7ce3158410e7231e01b14bf467d364
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-03-15 00:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (40.71%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        
+        if(digits.empty()) return {};
+        
+        vector<string> mapping = {
+            "", "", "abc", "def", "ghi", 
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+        
+        vector<string> result;
+        string current = "";
+        
+        function<void(int)> backtrack = [&](int index){
+            
+            if(index == digits.size()){
+                result.push_back(current);
+                return;
+            }
+            
+            string letters = mapping[digits[index] - '0'];
+            
+            for(char ch : letters){
+                current.push_back(ch);
+                backtrack(index + 1);
+                current.pop_back();   // backtrack
+            }
+        };
+        
+        backtrack(0);
+        return result;
+    }
+};
