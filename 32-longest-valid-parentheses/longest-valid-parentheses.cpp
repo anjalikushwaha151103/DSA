553ff40bb361eee83a0efa723dc2c99391b8e56b
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-03-26 17:13:51 +0530

    Time: 3 ms (40.13%) | Memory: 11.7 MB (49.54%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);  // Sentinel for base index
+        int maxLength = 0;
+
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                st.push(i);  // Push index of '('
+            } else {
+                st.pop();  // Pop the last '(' or sentinel
+                if (st.empty()) {
+                    st.push(i);  // Reset base index to current ')'
+                } else {
+                    maxLength = max(maxLength, i - st.top());
+                }
+            }
+        }
+
+        return maxLength;
+    }
+};
