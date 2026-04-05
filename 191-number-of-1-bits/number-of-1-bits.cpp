6860e5c83415a2b883d7eac9692b4a9da02c0c9d
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-04-05 20:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (81.13%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int cnt=0;
-        while(n!=0){
-            cnt++;
-            n=n&(n-1);
+        int count = 0;
+        while (n) {
+            n &= (n - 1); // clears the lowest set bit
+            ++count;
         }
-        return cnt;    
+        return count;
     }
 };
