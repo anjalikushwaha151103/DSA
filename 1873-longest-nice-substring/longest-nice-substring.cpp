3357f5406848b0663a953ba0bc9bd1d7904ce482
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-05-09 11:47:55 +0530

    Time: 327 ms (9.77%) | Memory: 111 MB (10.49%) - LeetSync

--- 1873-longest-nice-substring/longest-nice-substring.cpp
+++ 1873-longest-nice-substring/longest-nice-substring.cpp
@@ -2,41 +2,30 @@ class Solution {
 public:
     string longestNiceSubstring(string s) {
         int n=s.length();
-
-        vector<string> subs;
         
-        // all substrings
-        for(int i=0;i<n;i++){
-            for(int j=1;j<=n-i;j++){
+        for(int j=n;j>=1;j--){
+            for(int i=0;i<=n-j;i++){
                 string sub=s.substr(i,j);
-                subs.push_back(sub);
-            }
-        }
-        stable_sort(subs.begin(), subs.end(), [](const string& a, const string& b) {
-            return a.length() > b.length();
-        });
-
-        for(string st:subs){
-            unordered_set<char> c_set(st.begin(),st.end());
-            bool nice= true;
-            for(char ch:st){
-                if(islower(ch)){
-                    if(c_set.find(toupper(ch))==c_set.end()){
-                        nice=false;
-                        break;
-                    }
-                }else{
-                    if(c_set.find(tolower(ch))==c_set.end()){
-                        nice=false;
-                        break;
-                    }
-                }   
-            }
-            if(nice){
-                return st;
-                break;
+                if(is_nice(sub)) return sub;
             }
         }
         return "";
     }
+
+    bool is_nice(string st){
+        unordered_set<char> c_set(st.begin(),st.end());
+        for(char ch:st){
+            if(islower(ch)){
+                if(c_set.find(toupper(ch))==c_set.end()){
+                    return false;
+                }
+            }else{
+                if(c_set.find(tolower(ch))==c_set.end()){
+                    return false;
+                }
+            }   
+        }
+        return true;
+    }
+
 };
