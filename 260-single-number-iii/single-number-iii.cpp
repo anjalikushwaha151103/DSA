44062d36f61243123d9429f85f3c54e82a2b3e7b
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-05-11 21:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (54.72%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -6,11 +6,9 @@ public:
         for(int i=0; i<n; i++){
             xorr=xorr^nums[i];
         }
-
         unsigned int sb = (unsigned int)xorr & (-(unsigned int)xorr);
         int b1=0;
         int b2=0;
-
         for(int i=0; i<n; i++){
             if((nums[i]&sb)==0) b1=b1^nums[i];
             else b2=b2^nums[i];
