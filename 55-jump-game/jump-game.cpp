50893a4d4c3e75de42140b9d256382cea6acb621
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-05-11 21:30:46 +0530

    Simplify canJump logic
    Refactor canJump function for clarity and efficiency.

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int maxReach = 0;
-
         for(int i = 0; i < nums.size(); i++) {
-
-            // if current index is unreachable
             if(i > maxReach)
                 return false;
-
-            // update farthest reachable index
             maxReach = max(maxReach, i + nums[i]);
-
-            // already can reach or cross last index
             if(maxReach >= nums.size() - 1)
                 return true;
         }
-
         return true;
     }
 };
