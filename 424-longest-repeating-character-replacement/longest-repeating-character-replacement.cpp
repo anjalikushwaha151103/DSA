54f15aea2be5c547eb98233035ffbb67c64dcf10
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-05-14 18:22:17 +0530

    Time: 1 ms (74.29%) | Memory: 10.9 MB (39.35%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -6,22 +6,17 @@ public:
         int r=0;
         int max_len=0;
         int max_f=0;
-        // unordered_map<char,int> mp;
         int arr[26]={0};
         int chng=0;
-
         while(r<n){
-            
             arr[s[r]-'A']++;
             max_f=max(max_f,arr[s[r]-'A']);
             chng=(r-l+1)-max_f;
-
             while(chng>k){
                 arr[s[l]-'A']--;
                 l++;
                 chng=(r-l+1)-max_f;
             }
-
             max_len=max(max_len,(r-l+1));
             r++;
         }
