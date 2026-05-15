b60e5c5aad1dc1a8b0114f851e5fad55cbc80cfb
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-05-15 22:31:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (23.99%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,38 +1,49 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n=s.size();
-        int m=t.size();
+        int n = s.size();
+        int required = t.size();
 
-        if(m>n) return "";
+        if (required > n) return "";
 
-        int min_len=INT_MAX;
-        int start=-1;
-        int cnt=0;
-        int l=0;
-        int r=0;
+        int freq[256] = {0};
 
-        int arr[256]={0};
-        for(int k=0;k<m;k++) arr[t[k]]++;
+        for (char c : t)
+            freq[(unsigned char)c]++;
 
-        while(r<n){
-            if(arr[s[r]]>0) cnt++;
-            arr[s[r]]--;
-            
-            while(cnt==m){
-                if(min_len>(r-l+1)){
-                  min_len=(r-l+1);
-                  start=l;  
+        int l = 0;
+        int matched = 0;
+
+        int minLen = INT_MAX;
+        int start = 0;
+
+        for (int r = 0; r < n; r++) {
+
+            if (freq[(unsigned char)s[r]] > 0)
+                matched++;
+
+            freq[(unsigned char)s[r]]--;
+
+            while (matched == required) {
+
+                int windowLen = r - l + 1;
+
+                if (windowLen < minLen) {
+                    minLen = windowLen;
+                    start = l;
                 }
 
-                arr[s[l]]++;
-                if(arr[s[l]]>0) cnt--;
+                freq[(unsigned char)s[l]]++;
+
+                if (freq[(unsigned char)s[l]] > 0)
+                    matched--;
+
                 l++;
             }
-            r++;
         }
 
-        if(start==-1) return "";
-        return s.substr(start,min_len);   
+        return minLen == INT_MAX
+               ? ""
+               : s.substr(start, minLen);
     }
 };
