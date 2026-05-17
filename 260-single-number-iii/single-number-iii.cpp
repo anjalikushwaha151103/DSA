4a36062475cb330a33cf01a882764471451c72ff
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-05-17 08:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (97.12%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -3,16 +3,20 @@ public:
     vector<int> singleNumber(vector<int>& nums) {
         int n=nums.size();
         int xorr=0;
+
         for(int i=0; i<n; i++){
             xorr=xorr^nums[i];
         }
+
         unsigned int sb = (unsigned int)xorr & (-(unsigned int)xorr);
         int b1=0;
         int b2=0;
+
         for(int i=0; i<n; i++){
             if((nums[i]&sb)==0) b1=b1^nums[i];
             else b2=b2^nums[i];
         }
+        
         return {b1,b2};
     }
 };
