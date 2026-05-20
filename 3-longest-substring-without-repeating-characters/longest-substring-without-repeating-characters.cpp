fd9517f19f4194d11fee46cc9668d295b16128a4
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-05-20 22:43:28 +0530

    Time: 6 ms (66.98%) | Memory: 11.8 MB (66.13%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -6,17 +6,12 @@ public:
         int r=0;
         unordered_map <char,int> mp;
         int max_len=0;
-
         while(r<n){
-             if(mp.find(s[r]) != mp.end() && mp[s[r]] >= l) {
-                l = mp[s[r]] + 1;
-            }
+            if(mp.find(s[r]) != mp.end() && mp[s[r]] >= l) l = mp[s[r]] + 1;
             mp[s[r]]=r;
             max_len=max(max_len,(r-l+1));
             r++;
         }
-
         return max_len;
-        
     }
 };
