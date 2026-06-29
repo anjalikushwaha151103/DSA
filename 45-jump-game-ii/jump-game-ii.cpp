480b56bc51d67b68c9f1df9f7078767bff9f52e4
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-06-29 23:00:31 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (39.26%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -4,16 +4,13 @@ public:
         int jumps = 0;
         int currEnd = 0;
         int farthest = 0;
-
         for (int i = 0; i < nums.size() - 1; i++) {
             farthest = max(farthest, i + nums[i]);
-
             if (i == currEnd) {
                 jumps++;
                 currEnd = farthest;
             }
         }
-
         return jumps;
     }
 };
