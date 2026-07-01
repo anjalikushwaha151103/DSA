73e2e57466a390a41da940314847abe9055c3293
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-07-01 23:55:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (58.59%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        string doubled = s + s;
-
-        return doubled.find(goal) != string::npos;
+        string doubled=s+s;
+        return doubled.find(goal)!=string::npos;
     }
 };
