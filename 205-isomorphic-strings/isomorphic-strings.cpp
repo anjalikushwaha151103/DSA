33131a97f0ef85fbe485a9115519762cb58d7631
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-07-02 23:13:25 +0530

    Time: 4 ms (23.26%) | Memory: 9.4 MB (28.49%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+
+        if(n!=m) return false;
+
+        unordered_map<char,char>mp1;
+        unordered_map<char,char>mp2;
+
+        for(int i=0;i<n;i++){
+            if(mp1.find(s[i])==mp1.end() && mp2.find(t[i])==mp2.end() ){
+                mp1[s[i]]=t[i];
+                mp2[t[i]]=s[i];
+
+            }else{
+                if(mp1[s[i]]!=t[i] || mp2[t[i]]!=s[i]) return false;
+            }
+        }
+        return true;
+    }
+};
