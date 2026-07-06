f0eb9b8a4818775d7299a6c005517adcbfdf044c
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-07-06 19:11:36 +0530

    Time: 3 ms (94.32%) | Memory: 44.9 MB (48.03%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -6,14 +6,14 @@ public:
         
         int n=g.size();
         int m=s.size();
-        int cnt=0;
+        // int cnt=0;
 
         int i=0;
         int j=0;
         
         while(i<n && j<m){
             if(s[j]>=g[i]){
-                cnt++;
+                // cnt++;
                 i++;
                 j++;
             }else{
@@ -21,6 +21,6 @@ public:
             }
         }
 
-        return cnt;    
+        return i;    
     }
 };
