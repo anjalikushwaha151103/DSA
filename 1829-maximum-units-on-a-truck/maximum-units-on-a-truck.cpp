0a2e91efc60a6a7810f1238963b9a733dbf76281
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-07-06 23:14:32 +0530

    Time: 12 ms (20.88%) | Memory: 19.9 MB (66.02%) - LeetSync

---
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        int n=boxTypes.size();
+
+        sort(boxTypes.begin(),boxTypes.end(),[](const vector<int>& a, const       vector<int>& b) {
+            return a[1] > b[1];
+        });
+
+        int max_p=0;
+        for(int i=0;i<n;i++){
+            if(boxTypes[i][0]<=truckSize){
+                max_p+=boxTypes[i][0]*boxTypes[i][1];
+                truckSize-=boxTypes[i][0];
+            }else{
+                if(truckSize>0){
+                    max_p+=truckSize *boxTypes[i][1];
+                    return max_p;
+                }
+            }
+        }
+        return max_p;
+    }
+};
