afb5b166e2acd0e9a100b4e13dcb35dead064b10
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-07-16 23:38:07 +0530

    Time: 0 ms (100.00%) | Memory: 26.9 MB (40.25%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -5,9 +5,7 @@ public:
         int n=h.size();
         int left_max=h[0];
         vector <int> suf_max(n);
-
-
-
+        
         suf_max[n-1]=h[n-1];
         for(int i=n-2;i>=0;i--){
             suf_max[i]=max(h[i],suf_max[i+1]);     
