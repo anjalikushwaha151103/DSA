4e02596a545de7a7d87ac659fa74ae7016602065
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-08-08 22:36:27 +0530

    Time: 78 ms (22.52%) | Memory: 110 MB (23.78%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -8,11 +8,9 @@ public:
     }
 
     void f(int n, int k,vector<vector<int>> &ans,vector<int> &ds,int i){
-        if(i>n){
-
-            if(k==0){
-                ans.push_back(ds);
-            }
+        
+        if(k==0){
+            ans.push_back(ds);
             return;
         }
 
