217a3958f58278da51c2b8542cfe4852dac1d257
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-08-19 12:14:26 +0530

    Time: 14 ms (10.74%) | Memory: 20.1 MB (33.20%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        return topoSort(numCourses,prerequisites);
+    }
+
+    bool topoSort(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj (V);
+        vector<int> indegree(V);
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+        
+        vector<int> visited(V,0);
+        int cnt=0;
+        queue<int> q;
+        
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            cnt++;
+            
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+        
+        
+        if(cnt==V) return true;
+        return false;
+    }
+};
