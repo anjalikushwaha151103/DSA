048d3428e88506d3e40836112f27828492403dac
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-08-20 22:38:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (38.96%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n <= 2) return n;
+        vector<int> dp(n+1,-1);
+        return f(n,dp);
+    }
 
-        int prev1 = 2;   // ways to reach step 2
-        int prev2 = 1;   // ways to reach step 1
-        int current;
-
-        for (int i = 3; i <= n; i++) {
-            current = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = current;
-        }
-
-        return prev1;
+    int f(int n, vector<int>&dp){
+        if(n==0 || n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=f(n-1,dp) +f(n-2,dp);
     }
 };
