8a07025bf64faa5a533f6a317e1879027f6ac949
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-08-25 00:30:48 +0530

    Time: 2 ms (20.86%) | Memory: 9.5 MB (31.11%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+       vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+       return f(m,n,dp); 
+    }
+
+    int f(int m , int n,vector<vector<int>> &dp){
+        if(m==1|| n==1) return dp[m][n]= 1;
+        if(dp[m][n]!=-1) return dp[m][n];
+        return dp[m][n]=f(m,n-1,dp)+f(m-1,n,dp);
+    }
+};
