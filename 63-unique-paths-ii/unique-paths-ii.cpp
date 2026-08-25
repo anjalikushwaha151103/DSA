604a13117bcab4bd63e00a2cbc0d14cf5f51ef38
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-08-25 22:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (6.57%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -5,21 +5,24 @@ public:
         int n=obstacleGrid[0].size();
         vector<vector<int>> dp(m,vector<int>(n,-1));
 
-        return f(m-1,n-1,obstacleGrid,dp);
-    }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]= 0;
+                    continue;
+                }
+                else if(i==0 && j==0) dp[0][0]=1;
+                else{
+                    int up=0;
+                    int left=0;
+                    if(i-1>=0) up+=dp[i-1][j];
+                    if(j-1>=0) left+=dp[i][j-1];
+                    dp[i][j]= up+left;
+                }
+            }
+        }
 
-    int f(int i,int j,vector<vector<int>>& grid ,
-    vector<vector<int>>& dp){
-        if(i>=0 && j>=0 && grid[i][j]==1) return dp[i][j]= 0;
-        if(i<0 || j<0) return 0;
-        if(i==0 && j==0) return dp[i][j]=1;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int up=0;
-        int left=0;
-        if(i-1>=0) up+=f(i-1,j,grid,dp);
-        if(j-1>=0) left+=f(i,j-1,grid,dp);
-        return dp[i][j]= up+left;
+        return dp[m-1][n-1];
     }
 };
