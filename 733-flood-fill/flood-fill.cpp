ea839db54580d11fe497d3910347b007c719f81d
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-08-30 18:14:50 +0530

    Time: 3 ms (12.59%) | Memory: 18.3 MB (30.45%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m=image[0].size();
+        int m=image.size();
+        int n=image[0].size();
 
-        int target=image[sr][sc];
-        if(target == color) return image;
-        bfs(sr,sc,image,target,color);
-
-        return image;
-
-    }
-
-    void bfs(int i, int j,vector<vector<int>>& image,int target,int color){
-        int n=image.size();
-        int m=image[0].size();
         queue<pair<int,int>> q;
-        q.push({i,j});
-        image[i][j]=color;
+        q.push({sr,sc});
 
-        int dr[] = {-1, 0, 1, 0};
-        int dc[] = {0, 1, 0, -1};
+        int old=image[sr][sc];
+        if(old==color) return image;
+        image[sr][sc]=color;
+
+        vector<int> dr={-1,1,0,0};
+        vector<int> dc={0,0,-1,1};
 
         while(!q.empty()){
             int i=q.front().first;
             int j=q.front().second;
             q.pop();
 
-
             for(int k=0;k<4;k++){
                 int row=i+dr[k];
                 int col=j+dc[k];
-                if(row>=0 && row<n && col>=0 && col<m){
-                    if(image[row][col]== target){
-                        image[row][col]=color;
-                        q.push({row,col});
-                    }
+
+                if(row>=0 && row<m && col>=0 && col<n && image[row][col]==old){
+                    image[row][col]=color;
+                    q.push({row,col});
                 }
+
             }
-            
+
         }
+
+        return image;
     }
 };
