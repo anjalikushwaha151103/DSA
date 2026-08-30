7a2dbf97a106e304d48eeff5b42f263e620c46fd
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-08-30 18:46:12 +0530

    Time: 61 ms (6.51%) | Memory: 25.4 MB (7.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -4,54 +4,33 @@ public:
         int m=grid.size();
         int n=grid[0].size();
 
-        vector<vector<int>> visited(m,vector<int>(n,0));
-
+        vector<vector<int>> visited(m,vector<int> (n,0));
         int cnt=0;
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(visited[i][j]==0 && grid[i][j]=='1'){
+                if(!visited[i][j] && grid[i][j]=='1'){
                     cnt++;
-                    bfs(i,j,visited,grid);
+                    dfs(i,j,visited,m,n,grid);
+                    
                 }
             }
         }
-
         return cnt;
     }
 
-
-    void bfs(int i,int j,vector<vector<int>> &visited, vector<vector<char>> &grid){
-        int m=grid.size();
-        int n=grid[0].size();
-
-        queue<pair<int,int>> q;
-        q.push({i,j});
+    void dfs(int i,int j,vector<vector<int>> &visited,int m,int n,vector<vector<char>> &grid){
         visited[i][j]=1;
-
         vector<int> dr={0,0,-1,1};
         vector<int> dc={-1,1,0,0};
 
-        while(!q.empty()){
-            int a=q.front().first;
-            int b=q.front().second;
+        for(int k=0;k<4;k++){
+            int row=i+dr[k];
+            int col=j+dc[k];
 
-            q.pop();
-
-
-            for(int k=0;k<4;k++){
-                int row=a+dr[k];
-                int col=b+dc[k];
-
-                if(row>=0 && row<m && col>=0 && col<n){
-                    if(visited[row][col]==0 && grid[row][col]=='1'){
-                        visited[row][col]=1;
-                        q.push({row,col});   
-                    }
-                }
-
+            if(row>=0 && row<m && col>=0 && col<n && grid[row][col]=='1' && visited[row][col]==0 ){
+                dfs(row,col,visited,m,n,grid);
             }
         }
-
+        
     }
 };
