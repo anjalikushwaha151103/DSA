83bfdc4a3bef2ffe7fd93f93cc559a4d2c5513b2
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-08-30 17:05:43 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (69.43%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,54 +4,45 @@ public:
         int m=grid.size();
         int n=grid[0].size();
 
-        vector<vector<int>>visited (m,vector<int>(n,0));
-        queue<pair<pair<int,int>,int>> q;
+        queue<pair<int,pair<int,int>>> q;
+
+        //push rotten oranges into the queue
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    visited[i][j]=2;
-                }
+                if(grid[i][j]==2) q.push({0,{i,j}});
             }
         }
 
-        vector<int> dr={0,0,-1,1};
-        vector<int> dc={-1,1,0,0};
-
-        int ans=0;
+        int t=0;
+        vector<int> dr={-1,1,0,0};
+        vector<int> dc={0,0,-1,1};
         while(!q.empty()){
-            pair node=q.front().first;
-            int t=q.front().second;
-
-            int i=node.first;
-            int j=node.second;
-
+            int i=q.front().second.first;
+            int j=q.front().second.second;
+            int time=q.front().first;
             q.pop();
 
             for(int k=0;k<4;k++){
                 int row=i+dr[k];
                 int col=j+dc[k];
 
-                if(row>=0 && row<m && col>=0 && col<n){
-                    if(visited[row][col]!=2 && grid[row][col]==1){
-                        visited[row][col]=2;
-                        q.push({{row,col},t+1});
-                    }
+                if(row>=0 && row<m && col>=0 && col<n && grid[row][col]==1){
+                    grid[row][col]=2;
+                    q.push({time+1,{row,col}});
                 }
 
+                t=max(t,time);
             }
-
-            ans=max(ans,t);
-            
         }
+
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && visited[i][j]==0) return -1;
+                if(grid[i][j]==1) return -1;
             }
         }
-
-        return ans;
+        
+        return t;
     }
 };
