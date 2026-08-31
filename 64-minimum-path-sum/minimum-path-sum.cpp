7e00aefa97dd4e083aeaee36b182f5877a8f3710
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-08-31 15:27:33 +0530

    Time: 3 ms (30.11%) | Memory: 16.4 MB (16.23%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -4,21 +4,23 @@ public:
         int m=grid.size();
         int n=grid[0].size();
 
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,vector<int>(n,0));
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]= grid[0][0];
+                else{
+                    int up=1e9;
+                    int left=1e9;
+                    if(i-1>=0) up=grid[i][j]+dp[i-1][j];
+                    if(j-1>=0) left=grid[i][j]+dp[i][j-1];
+
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
         
-        return f(m-1,n-1,grid,dp);
+        return dp[m-1][n-1];
 
-    }
-
-    int f(int i,int j,vector<vector<int>>& grid ,vector<vector<int>> &dp){
-        if(i==0 && j==0) return grid[0][0];
-        if(i<0 || j<0) return 1e9;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int up=grid[i][j]+f(i-1,j,grid,dp);
-        int left=grid[i][j]+f(i,j-1,grid,dp);
-
-        return dp[i][j]=min(up,left);
     }
 };
