aa5f786649e77f19e4df6ee7b050cd4a4f513c36
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-09-01 08:10:51 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (95.51%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -18,14 +18,14 @@ public:
         q.push(root);
 
         while(!q.empty()){
+            int s=q.size();
             vector<int> level;
-            int n=q.size();
-            for(int i=0;i<n;i++){
+            for(int i=0;i<s;i++){
                 TreeNode* node=q.front();
-                if(node->left!=NULL) q.push(node->left);
-                if(node->right!=NULL) q.push(node->right);
-                level.push_back(node->val);
                 q.pop();
+                level.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
             }
             ans.push_back(level);
         }
