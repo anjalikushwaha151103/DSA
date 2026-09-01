d9650741c3cd2c6009d3f1d15984068c20ea60e5
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-09-01 16:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (7.00%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -15,42 +15,31 @@ public:
         vector<int> ans;
         if(root==NULL) return ans;
 
-        queue<TreeNode*> q;
-        q.push(root);
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        q.push({root,{0,0}});
+
+        map<int,pair<int,int>> mp;
 
         while(!q.empty()){
-            vector<int> level;
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                TreeNode* node=q.front();
-                if(node->left!=NULL) q.push(node->left);
-                if(node->right!=NULL) q.push(node->right);
-                level.push_back(node->val);
-                q.pop();
-            }
-            ans.push_back(level.back());
+            TreeNode* node=q.front().first;
+            int i=q.front().second.first;
+            int j=q.front().second.second;
+            
+            // if(mp.find(j)==mp.end())  
+            mp[j]={i,node->val};
+
+
+            q.pop();
+
+            if(node->left) q.push({node->left,{i-1,j+1}});
+            if(node->right) q.push({node->right,{i+1,j+1}});
+
         }
-        
-        // queue<pair<TreeNode*,int>> q;
-        // map<int,TreeNode*> mp;
-        
-        // q.push({root,0});
-        
-        // while(!q.empty()){
-        //     TreeNode* node=q.front().first;
-        //     int v=q.front().second;
-            
-        //     mp[abs(v)]=node;
-            
-        //     if(node->left) q.push({node->left,v-1});
-        //     if(node->right) q.push({node->right,v+1});
-            
-        //     q.pop();
-        // }
-        
-        // for(auto it:mp){
-        //     ans.push_back(it.second->val);
-        // }
+
+
+        for(auto it:mp){
+            ans.push_back(it.second.second);
+        }
 
         return ans;
     }
