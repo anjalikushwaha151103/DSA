aa2880358681ba56307547a8a8fc6dd93f096d16
Anjali Kushwaha <anjalikushwaha8009@gmail.com>
2026-09-04 23:50:49 +0530

    Time: 16 ms (65.80%) | Memory: 38.8 MB (37.86%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -4,52 +4,48 @@ public:
         int m=mat.size();
         int n=mat[0].size();
 
-        vector<vector<int>>visited (m,vector<int>(n,0));
+        vector<vector<int>> visited(m,vector<int>(n,0));
+        vector<vector<int>> dist(m,vector<int>(n,0));
+
         queue<pair<pair<int,int>,int>> q;
 
-        vector<vector<int>>ans (m,vector<int>(n,0));
-
+        //all 1's
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
                     q.push({{i,j},0});
                     visited[i][j]=1;
-                    ans[i][j]=0;
                 }
             }
         }
 
+        //dr & dc
 
         vector<int> dr={0,0,-1,1};
         vector<int> dc={-1,1,0,0};
 
+        //cal dist
         while(!q.empty()){
-            pair node=q.front().first;
-            int t=q.front().second;
+            int i=q.front().first.first;
+            int j=q.front().first.second;
 
-            int i=node.first;
-            int j=node.second;
+            int d=q.front().second;
 
+            dist[i][j]=d;
 
             q.pop();
-            
 
             for(int k=0;k<4;k++){
                 int row=i+dr[k];
                 int col=j+dc[k];
 
-                if(row>=0 && row<m && col>=0 && col<n){
-                    if(visited[row][col]!=1 && mat[row][col]==1){
-                        visited[row][col]=1;
-                        q.push({{row,col},t+1});
-                        ans[row][col]=t+1;
-                    }
+                if(row>=0 && row<m && col>=0 && col<n && !visited[row][col] ){
+                    visited[row][col]=1;
+                    q.push({{row,col},d+1});
                 }
-
             }
-
-
         }
-        return ans;
+
+        return dist;
     }
 };
